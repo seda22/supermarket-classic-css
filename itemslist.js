028757900663b0b1245978fef52a798d8b742794
seda22 <sedakacarr@gmail.com>
2026-02-18 23:19:45 +0300

    Add itemslist.js data file and connect to App.jsx
    Created itemslist.js to store product arrays (fruits, cheeses, veggies).
    Defined 4 React items for each category.
    Imported these arrays into App.jsx to prepare dynamic CategoryRow rendering.

---
+++ itemslist.js
@@ -0,0 +1,4 @@
+export const fruits = ["Apple", "Strawberry", "Mango", "Banana"];
+export const cheeses = ["Mozzarella", "Parmesan", "Burrata", "Brie"];
+export const veggies = ["Carrot", "Tomato", "Sweet Potato", "Lettuce"];
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import {fruits, cheeses, veggies} from "./itemslist.js"
 export default function App() {
   return(
     <h1>EXTRA SUPERMARKET</h1>
