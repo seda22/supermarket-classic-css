45270d724c3f899491b857acfe8407d5f54797c4
seda22 <sedakacarr@gmail.com>
2026-02-18 23:35:09 +0300

    feat: create reusable CategorieRow component and integrate into App.jsx
    Implemented a reusable component to display products by category.
    Passed title and items as props and rendered dynamically using map.
    Integrated the component into App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import {fruits, cheeses, veggies} from "./itemslist.js"
+import CategorieRow from "./components/CategorieRow.jsx"
+import {fruits, cheeses, veggies} from "../itemslist.js"
+
 export default function App() {
   return(
+    <div>
     <h1>EXTRA SUPERMARKET</h1>
-    <CategorieRow />
-    <CategorieRow />
-    <CategorieRow />
+    <CategorieRow title="Delicious Fruits" items={fruits}/>
+    <CategorieRow title="Italian Cheeses" items={cheeses}/>
+    <CategorieRow title="Fresh Vegetables" items={veggies}/>
+    </div>
   )
 };
--- src/components/CategorieRow.jsx
+++ src/components/CategorieRow.jsx
@@ -1,5 +1,11 @@
-export default function CategorieRow () {
+export default function CategorieRow (props) {
+    const {title, items} = props;
     return(
-        
+        <div>
+            <h2>{title}</h2>
+            {items.map((item)=>{
+                <ItemCard name={item} key={item}/>
+            })}
+        </div>
     )
 }
